8dc3e22abdfed678d280e9bbb0f9c404f00be821
John Murphy <jmurphy5613@gmail.com>
2022-04-04 18:10:32 -0400

    sync

--- server/contracts/BillionaireBattles.sol
+++ server/contracts/BillionaireBattles.sol
@@ -6,11 +6,40 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
  
 contract BillionaireBattles is ERC721 {
 
-    uint listingFee;
+    uint listingTransactionFee; //fee taken from each listing
     uint reviveCost;
 
     constructor() ERC721("Billionaire Battles", "BBT") {
-        listingFee = 0.05;
+        listingTransactionFee = 0.05;
         reviveCost = 0.05;
+    }
+
+    struct character {
+        //character bio
+        string name;
+        string description;
+        string image;
+
+        uint health;
+        uint maxHealth;
+
+        string attack1Name;
+        string attack2Name;
+        string attack3Name;
+        uint attackDamageAttack1;
+        uint attackDamageAttack2;
+        uint attackDamageAttack3;
+    }
+
+    mapping(uint => character) characters;
+
+
+    // getters
+    function getListingFee() public view returns (uint) {
+        return listingFee;
+    }
+
+    function getReviveCost() public view returns (uint) {
+        return reviveCost;
     }
 }
